1851b440c3ebdc20605e22bf8dfe644917c9035b
saadabban76 <115649011+saadabban76@users.noreply.github.com>
2023-05-19 23:48:06 +0530

    second commit

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+var exphbs = require("express-handlebars");
+
+const path = require("path");
 const app = express();
 const port = 3000;
-
-const engine = require('express-handlebars');
 
 // ! this below code ensures that this project uses all the files inside static, in other words it joins the static folder the express js server
 // ? path.join() : join() is a method which joins the (/ or \) to the path folder/file regardless of any operating system
 // ? app.use() : use() is a middleware(a layer of code that sits between the server and the application's logic. It intercepts and processes incoming requests and outgoing responses, allowing for additional functionality and processing. It is commonly used for tasks such as authentication, request parsing, logging, and error handling.)
 app.use(express.static(path.join(__dirname, "static")));
-app.use('/', require(path.join(__dirname, "routes/blog.js")));
+// app.use("/", require(path.join(__dirname, "routes/blog.js")));
 
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
+var hbs = exphbs.create({
+  // Specify helpers which are only registered on this instance.
+});
 
-// app.get('/', (req,res)=> {
-//     res.sendFile();
-// })
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  res.render("./home");
+});
 
 app.listen(port, () => {
-    console.log("server listening on port 3000");
-})
+  console.log("server listening on port 3000");
+});
--- routes/blog.js
+++ routes/blog.js
@@ -1,20 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const path = require('path');
+const path = require("path");
 const app = express();
-const blogs = require('../data/blogs');
+const blogs = require("../data/blogs");
 
-router.get('/', (req,res)=> {
-    res.sendFile(path.join(__dirname, "../templates/index.html"));
-})
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/index.html"));
+});
 
-router.get('/blogs/:slug', (req,res)=>{
-    let ihi = blogs.filter((blog)=>{
-        return blog.slug == req.params.slug
-    })
-    console.log('hi : ', ihi);
-    res.sendFile(path.join(__dirname, "../templates/blogs.html"));
-})
+router.get("/blogs/:slug", (req, res) => {
+  let ihi = blogs.filter((blog) => {
+    return blog.slug == req.params.slug;
+  });
+  console.log("hi : ", ihi);
+  res.sendFile(path.join(__dirname, "../views/blogs.html"));
+});
 
-
-module.exports = router
+module.exports = router;
